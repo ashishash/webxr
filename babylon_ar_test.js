31bf268f4bae90eb010098d04ddf397c7955ba81
ashishash <ash21411@outlook.com>
2021-08-09 20:19:19 +0530

    Update babylon_ar_test.js

--- babylon_ar_test.js
+++ babylon_ar_test.js
@@ -1,4 +1,36 @@
 // 360 Image logic
+BABYLON.DefaultLoadingScreen.prototype.displayLoadingUI = function () {
+    if (document.getElementById("customLoadingScreenDiv")) {
+        // Do not add a loading screen if there is already one
+        document.getElementById("customLoadingScreenDiv").style.display = "initial";
+        return;
+    }
+    this._loadingDiv = document.createElement("div");
+    this._loadingDiv.id = "customLoadingScreenDiv";
+    this._loadingDiv.innerHTML = "CL Diode Laser Machine is Currently Loading";
+    var customLoadingScreenCss = document.createElement('style');
+    customLoadingScreenCss.type = 'text/css';
+    customLoadingScreenCss.innerHTML = `
+    #customLoadingScreenDiv{
+        background-color: #BB464Bcc;
+        color: white;
+        font-size:50px;
+        text-align:center;
+    }
+    `;
+    document.getElementsByTagName('head')[0].appendChild(customLoadingScreenCss);
+    this._resizeLoadingUI();
+    window.addEventListener("resize", this._resizeLoadingUI);
+    document.body.appendChild(this._loadingDiv);
+};
+
+BABYLON.DefaultLoadingScreen.prototype.hideLoadingUI = function(){
+    document.getElementById("customLoadingScreenDiv").style.display = "none";
+    console.log("scene is now loaded");
+}
+
+
+
 window.addEventListener('DOMContentLoaded', function(){
     var canvas = document.getElementById("renderCanvas");
 
@@ -9,20 +41,23 @@ window.addEventListener('DOMContentLoaded', function(){
     
     
     var createScene = async function () {
+         engine.displayLoadingUI();
         var pushButtonCore;
         var index = 0; 
     
         var scene = new BABYLON.Scene(engine);
+        scene.clearColor = new BABYLON.Color3(1, 1, 0.6);
         var camera = new BABYLON.ArcRotateCamera("cam", Math.PI/2, Math.PI / 2, 3, new BABYLON.Vector3(0,0.5,0));
         var light = new BABYLON.HemisphericLight("sun", new BABYLON.Vector3(0,1,0), scene);
         var anchor = new BABYLON.TransformNode("");
+       
         
         camera.wheelDeltaPercentage = 0.01;
         camera.attachControl(canvas, true, false);
         camera.lowerRadiusLimit = 1.5;
         camera.upperRadiusLimit = 5;
         camera.inputs.removeByType('ArcRotateCameraPointersInput');
-        
+      
         let chair;
       //  BABYLON.SceneLoader.ImportMesh(
         //  https://www.dropbox.com/s/o5w0nlstfj21202/bDraco.glb?dl=0
@@ -33,6 +68,7 @@ window.addEventListener('DOMContentLoaded', function(){
                 chair = mesh[0].getChildTransformNodes(false)[0];
                mesh.rotation = new BABYLON.Vector3(0, 180, 0);
                 chair.isVisible = false;
+             engine.hideLoadingUI();
         });
     
         // var xr = await scene.createDefaultXRExperienceAsync({floorMeshes: []})
